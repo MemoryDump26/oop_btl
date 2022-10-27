ffe91a4ad7877aba0fcf224db4872179e61209ce
Memory Dump <21020608@vnu.edu.vn>
2022-10-28 01:05:57 +0700

    omg lambdas and refs looks so good what the hell??

--- src/main/java/components/commands/EntityCommand.java
+++ src/main/java/components/commands/EntityCommand.java
@@ -5,66 +5,30 @@ import components.input.PlayerInputComponent;
 import entity.Entity;
 
 public class EntityCommand {
+    public static TargetedCommand<Entity> Up = Entity::moveUp;
+    public static TargetedCommand<Entity> Down = Entity::moveDown;
+    public static TargetedCommand<Entity> Left = Entity::moveLeft;
+    public static TargetedCommand<Entity> Right = Entity::moveRight;
+    public static TargetedCommand<Entity> Attack = Entity::attack;
 
-    public static TargetedCommand<Entity> Up = new TargetedCommand<>() {
-        @Override
-        public void execute(Entity e) {
-            e.moveUp();
-        }
-    };
-    public static TargetedCommand<Entity> Down = new TargetedCommand<>() {
-        @Override
-        public void execute(Entity e) {
-            e.moveDown();
-        }
-    };
-    public static TargetedCommand<Entity> Left = new TargetedCommand<>() {
-        @Override
-        public void execute(Entity e) {
-            e.moveLeft();
-        }
-    };
-    public static TargetedCommand<Entity> Right = new TargetedCommand<>() {
-        @Override
-        public void execute(Entity e) {
-            e.moveRight();
-        }
-    };
-    public static TargetedCommand<Entity> Attack = new TargetedCommand<>() {
-        @Override
-        public void execute(Entity e) {
-            e.attack();
+    public static TargetedCommand<Entity> BombPower = e -> {
+        if (e.getAttack() instanceof BombAttack b) {
+            int n = b.getNumOfBombs() + 1;
+            b.setNumOfBombs(n);
         }
     };
 
-    public static TargetedCommand<Entity> BombPower = new TargetedCommand<>() {
-        @Override
-        public void execute(Entity e) {
-            if (e.getAttack() instanceof BombAttack b) {
-                int n = b.getNumOfBombs() + 1;
-                b.setNumOfBombs(n);
-            }
+    public static TargetedCommand<Entity> FlamePower = e -> {
+        if (e.getAttack() instanceof BombAttack b) {
+            int n = b.getPower() + 1;
+            b.setPower(n);
         }
     };
 
-
-    public static TargetedCommand<Entity> FlamePower = new TargetedCommand<>() {
-        @Override
-        public void execute(Entity e) {
-            if (e.getAttack() instanceof BombAttack b) {
-                int n = b.getPower() + 1;
-                b.setPower(n);
-            }
-        }
-    };
-
-    public static TargetedCommand<Entity> SpeedPower = new TargetedCommand<>() {
-        @Override
-        public void execute(Entity e) {
-            if (e.getInput() instanceof PlayerInputComponent) {
-                double n = e.getSpeed() + 1;
-                e.setSpeed(n);
-            }
+    public static TargetedCommand<Entity> SpeedPower = e -> {
+        if (e.getInput() instanceof PlayerInputComponent) {
+            double n = e.getSpeed() + 1;
+            e.setSpeed(n);
         }
     };
 }
